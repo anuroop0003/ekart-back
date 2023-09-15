b4ee1cb4a17846ad7a88e118bfb866ef7efdb189
anuroopk <anuroop@metrictreelabs.com>
2023-09-15 18:09:05 +0530

    limit fix product api

--- Controller/productController.js
+++ Controller/productController.js
@@ -1,8 +1,9 @@
 const Products = require("../Schemas/productSchema");
 
 exports.listProducts = async (req, res) => {
+  const { skip_limit } = req.body;
   try {
-    const productsList = await Products.find();
+    const productsList = await Products.find().skip(skip_limit).limit(1000);
     res.status(200).json({ message: "Products fetched successfully", data: productsList, status: 200 });
   } catch (error) {
     res.status(500).json({ message: error.message, status: 500 });
@@ -20,17 +21,12 @@ exports.listCategories = async (req, res) => {
 };
 
 exports.listMainCategories = async (req, res) => {
-  const { category } = req.body;
-  const desiredCategories = ["selling_price"];
   try {
-    // `${category}`
     const selectedFields = "category_1 category_2"; // Space-separated list of fields to select
     const selectedProducts = await Products.find(
       { category_1: { $in: "category_1" } }, // Adjust the field name if needed
       selectedFields
     ).exec();
-
-    // console.log("Selected products:", selectedProducts);
     res.status(200).json({ message: "Products categories fetched successfully", data: selectedProducts, status: 200 });
   } catch (error) {
     res.status(500).json({ message: error.message, status: 500 });
