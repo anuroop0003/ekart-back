038777e88447c078f5b9cfffc09b16e60cdb4d33
anuroopk <anuroop@metrictreelabs.com>
2024-01-22 10:51:41 +0530

    smaple route added

--- api/index.js
+++ api/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const DatabaseConfig = require('../dbConfig');
 const bodyParser = require('body-parser');
 const userRoute = require('../router');
 const dotenv = require('dotenv').config().parsed;
 
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 DatabaseConfig();
 
 app.use('/api', userRoute);
+
+app.get('/', (req, res) => res.json('Server Started...........').status(200));
 
 app.listen(dotenv.Port, () => {
   console.log(`Server is running on http://localhost:${dotenv.Port}`);
