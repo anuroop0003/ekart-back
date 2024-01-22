2193da8558d49f9aa750411201589c47dbacae35
anuroopk <anuroop@metrictreelabs.com>
2024-01-22 09:49:12 +0530

    product service import fix

--- Controller/productController.js
+++ Controller/productController.js
@@ -1,4 +1,4 @@
-const productService = require('../Service/productService');
+const productService = require('../Service/productService.js');
 
 const productController = {
   addProduct: async (req, res) => {
