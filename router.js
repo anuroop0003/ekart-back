22637aba98b7343c6b6fcc1d855baa2fd67c5ee7
anuroopk <anuroop@metrictreelabs.com>
2024-01-22 11:34:45 +0530

    route all comment

--- router.js
+++ router.js
@@ -1,9 +1,9 @@
-const productController = require('./Controller/productController');
+// const productController = require('./Controller/productController');
 const express = require('express');
 const router = express.Router();
-const middleware = require('./Middleware/verifyToken');
-const categoryController = require('./Controller/categoryController');
-const subCategoryController = require('./Controller/subCategoryController');
+// const middleware = require('./Middleware/verifyToken');
+// const categoryController = require('./Controller/categoryController');
+// const subCategoryController = require('./Controller/subCategoryController');
 const userController = require('./Controller/userController');
 router.get('/', (req, res) =>
   res.json('Server Started...........').status(200)
@@ -15,42 +15,42 @@ router.post('/user/sign/up', userController.signUpUser);
 // User Routes
 
 // Category Routes
-router.post(
-  '/add/category',
-  middleware.verifyToken,
-  categoryController.addCategory
-);
-router.get(
-  '/list/category',
-  middleware.verifyToken,
-  categoryController.listCategory
-);
+// router.post(
+//   '/add/category',
+//   middleware.verifyToken,
+//   categoryController.addCategory
+// );
+// router.get(
+//   '/list/category',
+//   middleware.verifyToken,
+//   categoryController.listCategory
+// );
 // Category Routes
 
 // Sub Category Routes
-router.post(
-  '/add/category/sub',
-  middleware.verifyToken,
-  subCategoryController.addCategory
-);
-router.get(
-  '/list/category/sub',
-  middleware.verifyToken,
-  subCategoryController.listCategory
-);
+// router.post(
+//   '/add/category/sub',
+//   middleware.verifyToken,
+//   subCategoryController.addCategory
+// );
+// router.get(
+//   '/list/category/sub',
+//   middleware.verifyToken,
+//   subCategoryController.listCategory
+// );
 // Sub Category Routes
 
 // Product Routes
-router.post(
-  '/add/product',
-  middleware.verifyToken,
-  productController.addProduct
-);
-router.get(
-  '/list/product',
-  middleware.verifyToken,
-  productController.listProduct
-);
+// router.post(
+//   '/add/product',
+//   middleware.verifyToken,
+//   productController.addProduct
+// );
+// router.get(
+//   '/list/product',
+//   middleware.verifyToken,
+//   productController.listProduct
+// );
 // Product Routes
 
 module.exports = router;
